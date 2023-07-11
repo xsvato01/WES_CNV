1234e12d046f40690a3eb408287c663d73884c62
Jan Svatoñ <janek@keepers-iMac.local>
2023-07-11 09:27:33 +0200

    fixed typoos

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nextflow kuberun xsvato01/WES_CNV -pod-image 'cerit.io/nextflow/nextflow:22.11.1' -resume -params-file ./samplesheet.yaml
+nextflow kuberun xsvato01/WES_CNV -head-image 'cerit.io/nextflow/nextflow:22.11.1' -resume
